6162f6065fedcfb25707e3964c68d7de5d97369d
Italo1994 <italohortiz2015@gmail.com>
2022-12-18 21:11:05 -0300

    Subindo o projeto completo com uma falha na função de subtração

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+	padding: 20px;
+	border: 1px solid #CDCDCD;
+	backgrond-color: #B0C4DE;
+	color: #000;
+	font-size: 1.099rem;
+	font-weight: 700;
+	flex: 1;
+
+	&:hover {
+		opacity: 0.6;
+	}
+`
